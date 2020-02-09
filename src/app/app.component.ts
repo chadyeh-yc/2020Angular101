df461b3389699a0f2b96d1b2fdca76920856edda
ChadYeh <chadyeh.yc@outlook.com>
2020-02-09 15:54:33 +0800

    Modify DI HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -9,6 +10,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   key = '';
   title = 'demo1';
+
+  constructor(private http: HttpClient) {
+
+  }
 
   cleanKey() {
     this.key = '';
